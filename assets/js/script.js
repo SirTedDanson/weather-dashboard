ccd941640583e6f9fd45a23eececb6ff630975df
sirteddanson <knowledgeMED@gmail.com>
2021-12-03 18:03:22 -0500

    changed API for UVI

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // LAUNCH APPLICATION
 var searchCityWeather = function (searchedCity) {
-  getCurrent (searchedCity);
+  getGeo (searchedCity);
   getFiveDay (searchedCity);
 }
 
@@ -18,13 +18,37 @@ var resetPage = function () {
   }
 }
 
-// get current weather from API ----------------------------------------------------------------------
-var getCurrent = function (searchedCity) {
-  var currentWeather = "http://api.openweathermap.org/data/2.5/weather?q="+searchedCity+"&units=imperial&appid=d81dc018285004c32e878ad354aa6463";
+//----------------------------NEW API STRUCTURE---------------------//
+//-----GEO COORD-------//
+var getGeo = function (searchedCity) {
+  var currentWeather = "http://api.openweathermap.org/geo/1.0/direct?q="+searchedCity+"&limit=1&appid=d81dc018285004c32e878ad354aa6463";
   fetch(currentWeather).then(function(response) {
     if (response.ok) {
       response.json().then(function(data) {
-        currentWeatherParse (data);
+        geoDataParse (data, searchedCity);
+        console.log(data);
+      });
+    } else {
+      alert("Error: No response from weather API!");
+    }
+  });
+};
+
+var geoDataParse = function (geoData, searchedCity) {
+  var lon = geoData[0].lon
+  var lat = geoData[0].lat
+  console.log(lon)
+  console.log(lat)
+  getCurrent (lon, lat, searchedCity);
+}
+
+// get current weather from API ----------------------------------------------------------------------
+var getCurrent = function (lon, lat, searchedCity) {
+  var currentWeather = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=hourly,daily&appid=d81dc018285004c32e878ad354aa6463";
+  fetch(currentWeather).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        currentWeatherParse (data, searchedCity);
         console.log(data);
       });
     } else {
@@ -34,15 +58,17 @@ var getCurrent = function (searchedCity) {
 };
 
 // ---------- PARSE CURRENT WEATHER DATA ------------------
-var currentWeatherParse = function (currentData) {
+var currentWeatherParse = function (currentData, searchedCity) {
   // gather relavent current weather data
-  var currWeathCity = currentData.name;
-  var currWeathDay = new Date(currentData.dt*1000).toLocaleDateString("en-US");
-  var currWeathIcon = currentData.weather[0].icon;
-  var currWeathTemp = currentData.main.temp;
-  var currWeathWind = currentData.wind.speed;
-  var currWeathHumidity = currentData.main.humidity;
-  var currWeathUv = currentData.main.temp;
+  var currWeathCity = searchedCity
+  var currWeathDay = new Date(currentData.current.dt*1000).toLocaleDateString("en-US");
+  var currWeathIcon = currentData.current.weather[0].icon;
+  console.log(currWeathIcon)
+  var currWeathTemp = currentData.current.temp;
+  var currWeathWind = currentData.current.wind_speed;
+  var currWeathHumidity = currentData.current.humidity;
+  var currWeathUv = currentData.current.uvi;
+  console.log(currWeathUv)
   displayCurrent (currWeathCity, currWeathDay, currWeathIcon, currWeathTemp, currWeathWind, currWeathHumidity, currWeathUv)
 };
 
@@ -56,9 +82,12 @@ var displayCurrent = function (city, day, icon, temp, wind, humidity, uv) {
   var currentTemp = $("<p>").addClass("data title is-5 p-2 mb-0").text("Temp: " + temp + " \u00B0F");
   var currentWind = $("<p>").addClass("data title is-5 p-2 mb-0").text("Wind: " + wind + " MPH");
   var currentHumidity = $("<p>").addClass("data title is-5 p-2 mb-0").text("Humidity: " + humidity + " %");
+  var currentUvContainer = $("<div>").addClass("data title is-5 py-3 pl-3 mb-0 columns is-mobile")
+  var currentUvTitle = $("<p>").addClass("data title is-5 p-2 mb-0").text("UV Index: ");
   var currentUV = $("<p>").addClass("data title is-5 p-2 mb-0").text(uv);
   currentCity.append(currentIcon)
-  currentWeatherCard.append(currentCity, currentTemp, currentWind, currentHumidity, currentUV)
+  currentUvContainer.append(currentUvTitle ,currentUV)
+  currentWeatherCard.append(currentCity, currentTemp, currentWind, currentHumidity,  currentUvContainer)
   currentWeatherCont.append(currentWeatherCard)
 }
 
@@ -112,7 +141,7 @@ var cityHist = function (searchedCity) {
   var searchForm = $("#search-form")
   var buttonContainer = $("<div>").addClass("control my-4")
   var cityHistButton = $("<button>")
-    .addClass("button prev-city is-success is-outlined")
+    .addClass("button prev-city is-info is-outlined")
     .attr("type", "submit")
     .text(searchedCity)
   buttonContainer.append(cityHistButton)
